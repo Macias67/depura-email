fa8d9947a133dc7e6f640729bb77d82b79fd8680
Luis Macias <luismacias.angulo@gmail.com>
2014-08-05 16:19:33 -0500

    añadi modelo origen

---
+++ src/modelo/Origen.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Macias
+ */
+public class Origen {
+
+    private String origen;
+
+    public Origen() {}
+
+    public Origen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+}
