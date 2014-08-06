63e5ce2a34af93bd23832fcd8facab1a9200d8d7
Diego I. Rodriguez <ing.diegorod@gmail.com>
2014-08-06 15:52:42 -0500

    main corregido

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,20 +20,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //Principal.main(args);
-        
-        String nuevo_origen=StringValidation.validaTexto("outlook");
-        System.out.println("origen nuevo es "+nuevo_origen);
-        
-        try {
-            Origen org = new Origen(nuevo_origen);
-            RegistraOrigen checar = new RegistraOrigen(org);
-            
-            System.out.println("resultado main :"+checar.checarOrigen(org));
-        } catch (Exception e) {
-            System.out.println("Error: "+e);
-        }
-        
+        Principal.main(args);
     }
 
 }
