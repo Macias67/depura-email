ed844333a8983dc89e408d9a0e31739f0588943a
Diego I. Rodriguez <ing.diegorod@gmail.com>
2014-08-20 18:12:01 -0500

    añadida base de datos limpia y añadidos archivos de prueba

---
+++ src/assets/depura_email.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-08-2014 a las 01:11:10
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `depura_email`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `correos`
+--
+
+CREATE TABLE IF NOT EXISTS `correos` (
+  `id_correo` int(11) NOT NULL AUTO_INCREMENT,
+  `correo` varchar(100) NOT NULL,
+  `id_origen` int(3) NOT NULL,
+  `id_grupo` int(3) NOT NULL,
+  `habilitado` enum('true','false') NOT NULL DEFAULT 'true',
+  PRIMARY KEY (`id_correo`),
+  KEY `id_origen` (`id_origen`),
+  KEY `id_grupo` (`id_grupo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupos`
+--
+
+CREATE TABLE IF NOT EXISTS `grupos` (
+  `id_grupo` int(3) NOT NULL AUTO_INCREMENT,
+  `grupo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_grupo`),
+  KEY `grupo` (`grupo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `origenes`
+--
+
+CREATE TABLE IF NOT EXISTS `origenes` (
+  `id_origen` int(3) NOT NULL AUTO_INCREMENT,
+  `origen` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_origen`),
+  KEY `origen` (`origen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `correos`
+--
+ALTER TABLE `correos`
+  ADD CONSTRAINT `correos_ibfk_1` FOREIGN KEY (`id_origen`) REFERENCES `origenes` (`id_origen`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `correos_ibfk_2` FOREIGN KEY (`id_grupo`) REFERENCES `grupos` (`id_grupo`) ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
